c114331c6a190f11358b8de3026bda4ad5872d3d
Remi Darmedru <remi.darmedru@outlook.com>
2026-01-08 02:57:30 +0100

    [Remi] Jump Land Ellen

--- Assets/3DGamekit/Audio/CustomScripts/CS_AudioCustomEvent_Ellen.cs
+++ Assets/3DGamekit/Audio/CustomScripts/CS_AudioCustomEvent_Ellen.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CS_AudioCustomEvent_Ellen : MonoBehaviour
 {
     public AK.Wwise.Event MC_FT;
+    public AK.Wwise.Event MC_JUMP;
+    public AK.Wwise.Event MC_LAND;
     public GameObject AudioSource;
     
     public void MC_FT_Play(AnimationEvent animationEvent)
@@ -14,5 +16,15 @@ public class CS_AudioCustomEvent_Ellen : MonoBehaviour
             AkSoundEngine.SetSwitch("MC_FOOTSTEPS_STATE", animationEvent.stringParameter, AudioSource);
             MC_FT.Post(AudioSource);
         }
+    }
+
+    public void MC_JUMP_Play()
+    {
+        MC_JUMP.Post(AudioSource);
+    }
+
+    public void MC_LAND_Play()
+    {
+        MC_LAND.Post(AudioSource);
     }
 }
